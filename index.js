765b4d38d17566b833bca413d23d250584b18534
block-doc-au <neilc@cns-gc.com.au>
2022-08-24 21:40:12 +1000

    NC competed JS Arrays filter lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(drivers, theString){
+    let resultArray = drivers.filter(n => (n.toLowerCase()) == (theString.toLowerCase()));
+    console.log(`The result array is : ${resultArray}`);
+    return resultArray;
+ };
+
+ function fuzzyMatch(drivers, theString){
+    let resultArray = drivers.filter(n => (n.charAt(0)) == (theString.charAt(0)));
+    return resultArray;
+};
+
+function matchName(drivers, theString){
+    let resultArray = drivers.filter(n => (n.name) == (theString));
+    return resultArray;
+};
